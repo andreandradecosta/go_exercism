c3afa9bcb99680bfed8f93161443f27fdc24eeaa
Andre Costa <andre.andrade.costa@gmail.com>
2015-11-11 13:57:12 -0200

    Impl. of 5th exercism.

--- triangle/triangle.go
+++ triangle/triangle.go
@@ -2,16 +2,41 @@
 
 package triangle
 
-// Code this function.
-func KindFromSides(a, b, c float64) Kind
+import "math"
 
-// Notice it returns this type.  Pick something suitable.
-type Kind
+// Kind represents a type of a triangle
+type Kind string
 
-// Pick values for the following identifiers used by the test program.
-Equ // equilateral
-Iso // isosceles
-Sca // scalene
-NaT // not a triangle
+// Types of triangles
+const (
+	NaT Kind = "NaT"
+	Equ Kind = "Equ"
+	Iso Kind = "Iso"
+	Sca Kind = "Sca"
+)
 
-// Organize your code for readability.
+func validNumbers(a, b, c float64) bool {
+	return !math.IsNaN(a) &&
+		!math.IsNaN(b) &&
+		!math.IsNaN(c) &&
+		!math.IsInf(a, 1) &&
+		!math.IsInf(b, 1) &&
+		!math.IsInf(c, 1) &&
+		!math.IsInf(a, -1) &&
+		!math.IsInf(b, -1) &&
+		!math.IsInf(c, -1)
+}
+
+// KindFromSides returns the type of a triangle based on its sides
+func KindFromSides(a, b, c float64) Kind {
+	if !validNumbers(a, b, c) || a+b <= c || a+c <= b || b+c <= a {
+		return NaT
+	}
+	if a == b && b == c {
+		return Equ
+	}
+	if a == b || b == c || c == a {
+		return Iso
+	}
+	return Sca
+}
