7a5f93ce537245c11cd9b82bef2d3a6ee124a1cd
Andre Costa <andre.andrade.costa@gmail.com>
2015-11-17 13:59:46 -0200

    Alternate version using closures. Benchmarks: BenchmarkSquareOfSums    5000000               251 ns/op BenchmarkSumOfSquares    5000000               251 ns/op BenchmarkDifference      3000000               507 ns/op

--- difference-of-squares/difference_of_squares.go
+++ difference-of-squares/difference_of_squares.go
@@ -1,20 +1,28 @@
 package diffsquares
 
+type sum func(int)
+
+func iterate(n int, op sum) {
+	for i := 1; i <= n; i++ {
+		op(i)
+	}
+}
+
 //SquareOfSums return the square of the sum of the first n natural numbers.
 func SquareOfSums(n int) int {
 	s := 0
-	for i := 1; i <= n; i++ {
-		s += i
-	}
+	iterate(n, func(x int) {
+		s += x
+	})
 	return s * s
 }
 
 //SumOfSquares return the sum of the squares of the first n natural numbers.
 func SumOfSquares(n int) int {
 	s := 0
-	for i := 1; i <= n; i++ {
-		s += i * i
-	}
+	iterate(n, func(x int) {
+		s += x * x
+	})
 	return s
 }
 
